3f95f5a32337052863550b113c4da4296fd60c72
tim <shenyang@shenyangs-MacBook-Pro.local>
2018-09-25 19:26:07 +0800

    add nuxt

---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        asite
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+    <ApolloQuery
+      :query="require('../graphql/videos.gql')"
+    >
+      <div slot-scope="{ result: { loading, error, data } }">
+        <div v-if="error" class="error apollo">An error occured</div>
+
+        <template v-else-if="data">
+          <div
+            v-for="message of data.messages"
+            :key="message.id"
+            class="message"
+          >
+            {{ message.text }}
+          </div>
+        </template>
+      </div>
+    </ApolloQuery>
+  </section>
+  
+</template>
+
+<script>
+import AppLogo from "~/components/AppLogo.vue";
+
+export default {
+  components: {
+    AppLogo
+  }
+};
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
